bd2cefe2497fd72efe380e064eec2ec8404904d6
vermav27 <verma.vineet27@gmail.com>
2026-07-07 23:26:19 +0530

    headless implementation

--- src/main/java/org/example/driver/DriverManager.java
+++ src/main/java/org/example/driver/DriverManager.java
@@ -2,6 +2,7 @@ package org.example.driver;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.example.utils.PropertiesReader;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -28,24 +29,60 @@ public class DriverManager {
         String browser = PropertiesReader.readKey("browser");
         browser = browser.toLowerCase();
 
+        boolean headless = Boolean.parseBoolean(
+                System.getProperty("headless", "false"));
+
         switch (browser){
             case  "edge" :
                 EdgeOptions edgeOptions = new EdgeOptions();
                 edgeOptions.addArguments("--start-maximized");
                 edgeOptions.addArguments("--guest");
+
+                if (headless) {
+                    edgeOptions.addArguments("--headless=new");
+                }
+
                 driver = new EdgeDriver(edgeOptions);
+                if (headless) {
+                    driver.manage().window().setSize(new Dimension(1920, 1080));
+                } else {
+                    driver.manage().window().maximize();
+                }
+
                 break;
             case "chrome" :
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--start-maximized");
                 chromeOptions.addArguments("--guest");
+
+                if (headless) {
+                    chromeOptions.addArguments("--headless=new");
+                }
+
                 driver = new ChromeDriver(chromeOptions);
+                if (headless) {
+                    driver.manage().window().setSize(new Dimension(1920, 1080));
+                } else {
+                    driver.manage().window().maximize();
+                }
+
                 break;
             case "firefox" :
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 firefoxOptions.addArguments("--start-maximized");
                 firefoxOptions.addArguments("--guest");
+
+                if (headless) {
+                    firefoxOptions.addArguments("-headless");
+                }
+
                 driver = new FirefoxDriver(firefoxOptions);
+                if (headless) {
+                    driver.manage().window().setSize(new Dimension(1920, 1080));
+                } else {
+                    driver.manage().window().maximize();
+                }
+
                 break;
             default:
                 System.out.println("No Browser Supported !");
